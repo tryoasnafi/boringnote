f0df604c31293bb6aa86d100889bf95ca0d55261
Tryo Asnafi <tryoasnafi@gmail.com>
2023-05-18 15:56:02 +0700

    add table layout

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"strings"
 	"time"
+
+	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 type STATUS uint8
@@ -57,14 +59,36 @@ func (t *Tasks) add(name string) {
 	fmt.Printf("Task \"%s\" added \n", name)
 }
 
-// soft delete
-// func (t *Tasks) Remove(id uint16) {
-// 	for i, task := range *t {
-// 		if task.ID == id {
-// 			(*t)[i].DeletedAt = time.Now().Unix()
-// 		}
-// 	}
-// }
+func (t *Tasks) remove(id uint16) {
+	value, exists := (*t)[id]
+	if exists {
+		value.DeletedAt = time.Now().Unix()
+		(*t)[id] = value
+		saveTasks(TASK_FILENAME, *t)
+		fmt.Printf("Task \"%s\" removed \n", value.Name)
+		return
+	}
+	fmt.Println("Task not found")
+}
+
+func getTaskStatus(status STATUS) string {
+	switch status {
+	case TODO:
+		return "TODO"
+	case DOING:
+		return "DOING"
+	case ONTEST:
+		return "ONTEST"
+	case DONE:
+		return "DONE"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+func unixToTime(unix int64) string {
+	return time.Unix(unix, 0).Local().Format("2006-01-02 15:04:05")
+}
 
 func (t *Tasks) list() {
 	if (len(*t)) == 0 {
@@ -72,12 +96,26 @@ func (t *Tasks) list() {
 		return
 	}
 
+	taskRows := make([]table.Row, 100)
+
 	for i, task := range *t {
 		if task.DeletedAt != 0 {
 			continue
 		}
-		fmt.Printf("%d - %s\n", i, task.Name)
+		taskRows = append(taskRows, table.Row{i, task.Name, getTaskStatus(task.Status), unixToTime(task.CreatedAt), unixToTime(task.UpdatedAt)})
 	}
+
+	tab := table.NewWriter()
+	tab.SetOutputMirror(os.Stdout)
+	tab.AppendHeader(table.Row{"ID", "Task", "Status", "Created At", "Updated At"})
+	tab.AppendRows(taskRows)
+	tab.SortBy([]table.SortBy{
+		{Name: "ID", Mode: table.Asc},
+	})
+	tab.SetColumnConfigs([]table.ColumnConfig{
+		{Name: "Task", WidthMax: 40},
+	})
+	tab.Render()
 }
 
 func printHelp() {
@@ -110,6 +148,8 @@ func main() {
 		tasks.add(strings.Join(os.Args[2:], " "))
 	case "-l":
 		tasks.list()
+	case "-d":
+		tasks.remove(uint16(os.Args[2][0] - '0'))
 	case "-h":
 		printHelp()
 	default:
